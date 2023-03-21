1d1595c396e792667e2282091851225c553c13f1
HARSH <harshsinghreal@gmail.com>
2023-03-21 22:46:52 +0530

    added the tree code

--- trials.cpp
+++ trials.cpp
@@ -1,27 +1,6 @@
 #include"bits/stdc++.h"
 using namespace std; 
-int main(){
-    
-    int t;
-    cin>>t;
-    
-    while(t--){
-        int n,m;
-        cin>>n>>m;
-        string f,s;
-        cin>>f>>s;
-        
-        reverse(f.begin(),f.end());
-        s+=f;
-        
-        int count=0;
-        for(int i=0;i<n+m-1;i++){
-            if(s[i]==s[i+1]) count++;
-        }
-        
-        if(count<=1) cout<< "YES "<<endl;
-        else cout<< "NO"<<endl;
-    }
 
+int main(){
     return 0;
 }
